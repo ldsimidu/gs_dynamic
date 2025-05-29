07adc92656b259a4c68f22515c51768d34f7d0fa
Lucas Simidu <lucas.simidu@outlook.com>
2025-05-29 01:02:27 -0300

    feat: Adicionando funções fundamentais + base nova ação 'inserir_ocorrencia'

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import heapq
+import datetime
+
+dia_hoje = datetime.date.today()
+ocorrencias = []
+historico = []
+
 def input_nao_vazio(mensagem):
     while True:
         texto = input(mensagem).strip()
@@ -14,6 +21,35 @@ def forca_opcao(lista, mensagem):
         else:
             return escolha
 
+def escolher_regiao():
+    regioes = {
+        "1": "Norte",
+        "2": "Sul",
+        "3": "Leste",
+        "4": "Oeste",
+        "5": "Centro"
+    }
+    mensagem = (
+        "Informe a região da ocorrência:\n"
+        "1) Norte\n"
+        "2) Sul\n"
+        "3) Leste\n"
+        "4) Oeste\n"
+        "5) Centro\n"
+        "-> "
+    )
+
+    escolha = forca_opcao(list(regioes.keys()), mensagem)
+    regiao_escolhida = regioes[escolha]
+    return regiao_escolhida
+
+def formatar_ocorrencia(o):
+    return (f"🆔 {o['id']} \n🗺️ Região: {o['regiao']} \n🔥 Severidade: {o['severidade']} "
+            f"\n📄 {o['descricao']} \n⏰ {o['timestamp'].strftime('%d/%m/%Y %H:%M:%S')}")
+
+def inserir_ocorrencia():
+    print("opc1")
+
 def main_queimadas():
     print("-=" * 20)
     print("🔥  SIMULADOR DE RESPOSTA A QUEIMADAS 🔥")
@@ -29,7 +65,7 @@ def main_queimadas():
     opcao = forca_opcao(['0','1','2','3','4','5','6'], "Escolha uma opção:\n-> ")
 
     if opcao == '1':
-        print("opc1")
+        inserir_ocorrencia()
     elif opcao == '2':
         print("opc2")
     elif opcao == '3':
